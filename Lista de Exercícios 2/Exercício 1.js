0a1ed960f72110be9c336c14b0d9480c381a349a
ConhecedorX~ <Lucastation2@gmail.com>
2024-02-23 21:10:43 -0300

    Exercício 1 concluido

---
+++ Lista de Exercícios 2/Exercício 1.js
@@ -0,0 +1,8 @@
+function comprimentar(nome){
+    let nome1 = "Olá ".concat(nome)
+    return console.log(nome1.concat("!"))
+}
+
+comprimentar("Lucas")
+comprimentar('Larissa')
+comprimentar('Etevaldo')
