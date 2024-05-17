2e043dd05733a10468e79e77fc3abda1f8e47af7
ConhecedorX~ <Lucastation2@gmail.com>
2024-05-16 21:08:46 -0300

    .

--- Node/PastaA/PastaB/acessarModulos.js
+++ Node/PastaA/PastaB/acessarModulos.js
@@ -1,2 +1,14 @@
 const moduloA = require('../../ModuloA')
 console.log(moduloA.ola)
+
+const saudacao = require('Saudacao')
+console.log(saudacao.ola)
+
+const c = require('./PastaC/Index')
+console.log(c.ola2)
+
+const http = require('http')
+http.createServer ((req, res) => {
+    res.write('Boa noite Lucas!')
+    res.end()
+}).listen(8080)
